e5a40cc0a22ec4111e315900784ca362e395ae0c
vanshilshah <vanz4life@gmail.com>
2014-11-04 23:46:37 -0500

    Sample Demo to Test Myo API
    Goes through multiple steps of connecting with the Myo and displays
    recognized poses to the screen.

--- gen/com/example/sportshack/R.java
+++ gen/com/example/sportshack/R.java
@@ -38,13 +38,15 @@ public final class R {
         public static final int myosdk__firmware_incompatible_dot=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f090005;
-        public static final int myosdk__action_scan=0x7f090006;
-        public static final int myosdk__connection_state_dot=0x7f090003;
-        public static final int myosdk__firmware_incompatible_text=0x7f090001;
-        public static final int myosdk__fragment_scan=0x7f090000;
-        public static final int myosdk__progress=0x7f090002;
-        public static final int myosdk__scan_result_view=0x7f090004;
+        public static final int action_settings=0x7f090007;
+        public static final int button1=0x7f090000;
+        public static final int myosdk__action_scan=0x7f090008;
+        public static final int myosdk__connection_state_dot=0x7f090005;
+        public static final int myosdk__firmware_incompatible_text=0x7f090003;
+        public static final int myosdk__fragment_scan=0x7f090002;
+        public static final int myosdk__progress=0x7f090004;
+        public static final int myosdk__scan_result_view=0x7f090006;
+        public static final int status=0x7f090001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
