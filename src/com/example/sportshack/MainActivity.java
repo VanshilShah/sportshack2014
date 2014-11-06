f2be096ff54b42af96b1c321b58183ec21c9b406
vanshilshah <vanz4life@gmail.com>
2014-11-06 09:29:11 -0500

    Pitch, Roll, Yaw Integrated

--- src/com/example/sportshack/MainActivity.java
+++ src/com/example/sportshack/MainActivity.java
@@ -1,120 +1,188 @@
+/*
+* Copyright (C) 2014 Thalmic Labs Inc.
+* Distributed under the Myo SDK license agreement. See LICENSE.txt for details.
+*/
 package com.example.sportshack;
-
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.thalmic.myo.AbstractDeviceListener;
+import com.thalmic.myo.Arm;
 import com.thalmic.myo.DeviceListener;
 import com.thalmic.myo.Hub;
 import com.thalmic.myo.Myo;
 import com.thalmic.myo.Pose;
+import com.thalmic.myo.Quaternion;
+import com.thalmic.myo.XDirection;
 import com.thalmic.myo.scanner.ScanActivity;
-
-
 public class MainActivity extends Activity {
+// This code will be returned in onActivityResult() when the enable Bluetooth activity exits.
+private static final int REQUEST_ENABLE_BT = 1;
+private TextView mTextView;
+// Classes that inherit from AbstractDeviceListener can be used to receive events from Myo devices.
+// If you do not override an event, the default behavior is to do nothing.
 
-    private static final String TAG = "Error";
-    public static TextView status;
-
-	@Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        setContentView(R.layout.activity_main);
-        
-        status = (TextView) findViewById(R.id.status);
-        status.setBackgroundColor(Color.DKGRAY);
-        
-        Hub hub = Hub.getInstance();
-        if (!hub.init(this)) {
-            Log.e(TAG, "Could not initialize the Hub.");
-            status.setText("Could not initialize the Hub.");
-            finish();
-            return;
-        }else{
-        	status.setTextColor(Color.CYAN);
-            status.setText("Hub Initialized");
-        }
-        Hub.getInstance().addListener(mListener);
-        
-    }
-	
-	 @Override
-    protected void onResume() {
-        super.onResume();
-        // If Bluetooth is not enabled, request to turn it on.
-        if (!BluetoothAdapter.getDefaultAdapter().isEnabled()) {
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivity(enableBtIntent);
-        }
-    }
-	 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        // We don't want any callbacks when the Activity is gone, so unregister the listener.
-        Hub.getInstance().removeListener(mListener);
-        if (isFinishing()) {
-            // The Activity is finishing, so shutdown the Hub. This will disconnect from the Myo.
-            Hub.getInstance().shutdown();
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-    
-    public void findMyo(View view){
-    	Intent intent = new Intent(this, ScanActivity.class);
-    	this.startActivity(intent);
-    }
-    
-    private DeviceListener mListener = new AbstractDeviceListener() {
-        @Override
-        public void onConnect(Myo myo, long timestamp) {
-            Toast.makeText(MainActivity.this, "Myo Connected!", Toast.LENGTH_SHORT).show();
-            status.setTextColor(Color.GREEN);
-            status.setText("Myo Connected");
-        }
-
-        @Override
-        public void onDisconnect(Myo myo, long timestamp) {
-            Toast.makeText(MainActivity.this, "Myo Disconnected!", Toast.LENGTH_SHORT).show();
-            status.setTextColor(Color.RED);
-            status.setText("Myo Connected");
-        }
-
-        @Override
-        public void onPose(Myo myo, long timestamp, Pose pose) {
-            Toast.makeText(MainActivity.this, "Pose: " + pose, Toast.LENGTH_SHORT).show();
-            status.setTextColor(Color.GREEN);
-            status.setText(pose.toString());
-            //TODO: Do something awesome.
-        }
-    };
+private DeviceListener mListener = new AbstractDeviceListener() {
+private Arm mArm = Arm.UNKNOWN;
+private XDirection mXDirection = XDirection.UNKNOWN;
+// onConnect() is called whenever a Myo has been connected.
+@Override
+public void onConnect(Myo myo, long timestamp) {
+// Set the text color of the text view to cyan when a Myo connects.
+mTextView.setTextColor(Color.GREEN);
+}
+// onDisconnect() is called whenever a Myo has been disconnected.
+@Override
+public void onDisconnect(Myo myo, long timestamp) {
+// Set the text color of the text view to red when a Myo disconnects.
+mTextView.setTextColor(Color.RED);
+}
+// onArmRecognized() is called whenever Myo has recognized a setup gesture after someone has put it on their
+// arm. This lets Myo know which arm it's on and which way it's facing.
+@Override
+public void onArmRecognized(Myo myo, long timestamp, Arm arm, XDirection xDirection) {
+mArm = arm;
+mXDirection = xDirection;
+}
+// onArmLost() is called whenever Myo has detected that it was moved from a stable position on a person's arm after
+// it recognized the arm. Typically this happens when someone takes Myo off of their arm, but it can also happen
+// when Myo is moved around on the arm.
+@Override
+public void onArmLost(Myo myo, long timestamp) {
+mArm = Arm.UNKNOWN;
+mXDirection = XDirection.UNKNOWN;
+}
+// onOrientationData() is called whenever a Myo provides its current orientation,
+// represented as a quaternion.
+@Override
+public void onOrientationData(Myo myo, long timestamp, Quaternion rotation) {
+// Calculate Euler angles (roll, pitch, and yaw) from the quaternion.
+float roll = (float) Math.toDegrees(Quaternion.roll(rotation));
+float pitch = (float) Math.toDegrees(Quaternion.pitch(rotation));
+float yaw = (float) Math.toDegrees(Quaternion.yaw(rotation));
+// Adjust roll and pitch for the orientation of the Myo on the arm.
+if (mXDirection == XDirection.TOWARD_ELBOW) {
+roll *= -1;
+pitch *= -1;
+}
+// Next, we apply a rotation to the text view using the roll, pitch, and yaw.
+mTextView.setRotationX(roll);
+mTextView.setRotation(pitch);
+mTextView.setRotationY(yaw);
+}
+// onPose() is called whenever a Myo provides a new pose.
+@Override
+public void onPose(Myo myo, long timestamp, Pose pose) {
+// Handle the cases of the Pose enumeration, and change the text of the text view
+// based on the pose we receive.
+/*switch (pose) {
+case UNKNOWN:
+mTextView.setText(getString(R.string.hello_world));
+break;
+case REST:
+int restTextId = R.string.hello_world;
+switch (mArm) {
+case LEFT:
+restTextId = R.string.arm_left;
+break;
+case RIGHT:
+restTextId = R.string.arm_right;
+break;
+}
+mTextView.setText(getString(restTextId));
+break;
+case FIST:
+mTextView.setText(getString(R.string.pose_fist));
+break;
+case WAVE_IN:
+mTextView.setText(getString(R.string.pose_wavein));
+break;
+case WAVE_OUT:
+mTextView.setText(getString(R.string.pose_waveout));
+break;
+case FINGERS_SPREAD:
+mTextView.setText(getString(R.string.pose_fingersspread));
+break;
+case THUMB_TO_PINKY:
+mTextView.setText(getString(R.string.pose_thumbtopinky));
+break;
+}*/
+	 mTextView.setTextColor(Color.BLACK);
+     mTextView.setText(pose.toString());
+}
+};
+public void findMyo(View view){
+	Intent intent = new Intent(this, ScanActivity.class);
+	this.startActivity(intent);
+}
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+super.onCreate(savedInstanceState);
+setContentView(R.layout.activity_main);
+mTextView = (TextView) findViewById(R.id.status);
+// First, we initialize the Hub singleton with an application identifier.
+Hub hub = Hub.getInstance();
+if (!hub.init(this, getPackageName())) {
+// We can't do anything with the Myo device if the Hub can't be initialized, so exit.
+Toast.makeText(this, "Couldn't initialize Hub", Toast.LENGTH_SHORT).show();
+finish();
+return;
+}
+// Next, register for DeviceListener callbacks.
+hub.addListener(mListener);
+}
+@Override
+protected void onResume() {
+super.onResume();
+// If Bluetooth is not enabled, request to turn it on.
+if (!BluetoothAdapter.getDefaultAdapter().isEnabled()) {
+Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+}
+}
+@Override
+protected void onDestroy() {
+super.onDestroy();
+// We don't want any callbacks when the Activity is gone, so unregister the listener.
+Hub.getInstance().removeListener(mListener);
+if (isFinishing()) {
+// The Activity is finishing, so shutdown the Hub. This will disconnect from the Myo.
+Hub.getInstance().shutdown();
+}
+}
+@Override
+protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+// User chose not to enable Bluetooth, so exit.
+if (requestCode == REQUEST_ENABLE_BT && resultCode == Activity.RESULT_CANCELED) {
+finish();
+return;
+}
+super.onActivityResult(requestCode, resultCode, data);
+}
+@Override
+public boolean onCreateOptionsMenu(Menu menu) {
+super.onCreateOptionsMenu(menu);
+MenuInflater inflater = getMenuInflater();
+inflater.inflate(R.menu.main, menu);
+return true;
+}
+@Override
+public boolean onOptionsItemSelected(MenuItem item) {
+int id = item.getItemId();
+/*if (R.id.action_scan == id) {
+onScanActionSelected();
+return true;
+}*/
+return super.onOptionsItemSelected(item);
+}
 
 }
